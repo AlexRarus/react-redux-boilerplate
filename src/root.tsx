6a07884b38d52cf4969aa6d6cb476a6a12643289
Alex Serebryakov <avserebryakov@innotech.com>
2021-05-06 10:43:38 +0300

    fix packages versions

--- src/root.tsx
+++ src/root.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import DevTools from 'src/containers/dev-tools';
 import App from 'src/app';
 
 export default function Root() {
   return (
     <Switch>
       <Route path="/" render={() => <App />} />
-      {process.env.NODE_ENV === 'development' && <DevTools />}
     </Switch>
   );
 }
